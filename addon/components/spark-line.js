fb7bf5aa7174c8ae1f9e083129c41b35bae2318f
Selva thayalan <selvathayalan@admindroid.com>
2020-12-11 19:17:01 +0530

    added pieInstance and mouseLeave callback

--- addon/components/spark-line.js
+++ addon/components/spark-line.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import layout from '../templates/components/spark-line';
 
-const { on } = Ember;
+const { on,isPresent } = Ember;
 
 export default Ember.Component.extend({
     layout: layout,
@@ -9,25 +9,31 @@ export default Ember.Component.extend({
 
     classNames: ['sparkline'],
 
+    didInsertElement() {
+        this.drawSparkline();
+    },
 
-    initialize: on('didInsertElement', function() {
+    drawSparkline() {
+        var sparkline,
+        _this = this;
 
-        // var sparkline, _this = this;
+        let options = this.get('options') || {};
+        let data = this.get('data');
+        sparkline = this.$().sparkline(data, options);
 
-        // let options = this.get('options') || {};
-        // let data = this.get('data');
-        // sparkline = this.$().sparkline(data, options);
-
-        this.$().bind('sparklineClick', (ev) => {
-            this.set('sparklineClick', ev.sparklines[0]);
+        this.$().bind('sparklineClick', function(ev) {
+            _this.set('sparklineClick', ev.sparklines[0]);
         });
-        this.$().bind('sparklineRegionChange', (ev) => {
-            this.set('sparklineRegionChange', ev.sparklines[0]);
+        this.$().bind('sparklineRegionChange', function(ev) {
+            _this.set('sparklineRegionChange', ev.sparklines[0]);
         });
-    }),
+        
+        if(isPresent(this.attrs.pieInteractInstance))
+        this.attrs.pieInteractInstance.update(sparkline[0].interactInstance);//this instance is used for highlight the pie slice programatically using the handler instance provided with the sparkline instance.
+    },
 
     click() {
-        this.sendAction('action', this.get('sparklineClick'));
+        this.sendAction('sparklineClick', this.get('sparklineClick'));
     },
 
     mouseMove() {
@@ -39,7 +45,17 @@ export default Ember.Component.extend({
         }
     },
 
+    mouseLeave() {
+        if (this.get('hoverOut')) {
+            this.sendAction('hoverOut');
+        }
+    },
+
+    listenDataChanges: function () {
+        this.drawSparkline();
+    }.observes('data.length','data.[]'),
+
     destroySparkline: on('willDestroyElement', function() {
-        this.destroy();
+    this.destroy();
     }),
 });
